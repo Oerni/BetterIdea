b03a8631f4730ed4e345d9b0459a25bf6df1ad62
Stephan <Stephan@Zenbook.fritz.box>
2014-03-04 19:13:19 +0100

    login updated

--- BetterIdeaNeu/gen/com/example/betteridea/R.java
+++ BetterIdeaNeu/gen/com/example/betteridea/R.java
@@ -33,16 +33,15 @@ public final class R {
     public static final class id {
         public static final int TextView01=0x7f080003;
         public static final int TextView02=0x7f080002;
-        public static final int action_add=0x7f08000b;
-        public static final int action_search1=0x7f08000c;
-        public static final int action_settings=0x7f08000d;
-        public static final int button1=0x7f080007;
-        public static final int button2=0x7f08000a;
-        public static final int editText1=0x7f080005;
-        public static final int editText2=0x7f080006;
-        public static final int imageView1=0x7f080004;
-        public static final int imageView2=0x7f080008;
-        public static final int imageView3=0x7f080009;
+        public static final int action_add=0x7f08000a;
+        public static final int action_search1=0x7f08000b;
+        public static final int action_settings=0x7f08000c;
+        public static final int button1=0x7f080006;
+        public static final int button2=0x7f080009;
+        public static final int editText1=0x7f080004;
+        public static final int editText2=0x7f080005;
+        public static final int imageView2=0x7f080007;
+        public static final int imageView3=0x7f080008;
         public static final int linearLayout1=0x7f080000;
         public static final int textView2=0x7f080001;
     }
